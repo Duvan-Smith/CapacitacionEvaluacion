34a2aa4c20b6e0d677b2c3da6f8a65f6229ad92f
Duvan Smith Montenegro Roque <dsmith.mr@gmail.com>
2021-03-24 22:19:43 -0500

    BaseAplicacion

---
+++ Evaluacion.Aplicacion.Core/Base/Excepciones/EvaluacionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Evaluacion.Aplicacion.Core.Base.Excepciones
+{
+    public class EvaluacionException : Exception
+    {
+        public EvaluacionException()
+        {
+        }
+        public EvaluacionException(string message) : base(message)
+        {
+        }
+        public EvaluacionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
