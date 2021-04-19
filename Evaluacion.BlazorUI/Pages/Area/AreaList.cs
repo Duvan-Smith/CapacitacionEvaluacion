128c2b022d6bbdc2e8902ebc7aca13dc9f9b4a28
Duvan-Smith <59094271+Duvan-Smith@users.noreply.github.com>
2021-04-19 15:50:46 -0500

    Division de la logica y el html

---
+++ Evaluacion.BlazorUI/Pages/Area/AreaList.cs
@@ -0,0 +1,28 @@
+﻿using Evaluacion.Aplicacion.Dto.Genericas.Areas;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Evaluacion.BlazorUI.Pages.Area
+{
+    public partial class AreaList
+    {
+        private readonly string Url = "/FachadaGenericas/GetAllArea";
+        private IEnumerable<AreaDto> areaDtos;
+        private string _currentSelectedTask;
+        protected override async Task OnInitializedAsync() =>
+            areaDtos = await Http.GetFromJsonAsync<IEnumerable<AreaDto>>(Url).ConfigureAwait(false);
+        public void SelectionChangedEvent(object row)
+        {
+            if (row == null)
+            {
+                _currentSelectedTask = "";
+            }
+            else
+            {
+                _currentSelectedTask = string.Format("areaDtos Nr. {0} has been selected", ((AreaDto)row).Id);
+            }
+            this.StateHasChanged();
+        }
+    }
+}
---
+++ Evaluacion.BlazorUI/Pages/Cliente/ClienteList.cs
@@ -0,0 +1,29 @@
+﻿using Evaluacion.Aplicacion.Dto.Especificas.Clientes;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Evaluacion.BlazorUI.Pages.Cliente
+{
+    public partial class ClienteList
+    {
+        private readonly string Url = "/FachadaPersonas/GetAllCliente";
+        private IEnumerable<ClienteDto> clienteDtos;
+        private string _currentSelectedTask;
+
+        protected override async Task OnInitializedAsync() =>
+            clienteDtos = await Http.GetFromJsonAsync<IEnumerable<ClienteDto>>(Url).ConfigureAwait(false);
+        public void SelectionChangedEvent(object row)
+        {
+            if (row == null)
+            {
+                _currentSelectedTask = "";
+            }
+            else
+            {
+                _currentSelectedTask = string.Format("clienteDtos Nr. {0} has been selected", ((ClienteDto)row).Id);
+            }
+            this.StateHasChanged();
+        }
+    }
+}
